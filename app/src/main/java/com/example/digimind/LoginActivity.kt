90074753437ba61fc6636c5075c3ae89b52e5083
JesusGonzalez-173151 <jesusi.gonzalez@potros.itson.edu.mx>
2021-05-14 01:55:56 -0700

    digimind almacenamiento firestone

---
+++ app/src/main/java/com/example/digimind/LoginActivity.kt
@@ -0,0 +1,73 @@
+package com.example.digimind
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btn_registrarse: Button = findViewById(R.id.btn_registrarse)
+        val btn_contra: TextView = findViewById(R.id.tv_olvidasteContra)
+        val btn_ingresar: Button = findViewById(R.id.btn_ingresar)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_registrarse.setOnClickListener{
+            val intent: Intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_contra.setOnClickListener{
+            val intent: Intent = Intent(this, ContrasenaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_ingresar.setOnClickListener{
+            valida_ingreso()
+        }
+    }
+
+    private fun valida_ingreso(){
+        val et_correo: EditText = findViewById(R.id.et_correo)
+        val et_contra: EditText = findViewById(R.id.et_contra)
+
+        var correo: String = et_correo.text.toString()
+        var contra: String = et_contra.text.toString()
+
+        if(!correo.isNullOrBlank() && contra.isNullOrBlank()){
+            Toast.makeText(this, "Ingresar datos",
+                Toast.LENGTH_SHORT).show()
+        }else{
+            ingresaFirebase(correo,contra)
+        }
+    }
+    private fun ingresaFirebase(correo: String, contra:String){
+        auth.signInWithEmailAndPassword(correo, contra)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    // Log.d(TAG, "signInWithEmail:success")
+                    //val user = auth.currentUser
+                    //  updateUI(user)
+                    val intent: Intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this,"Exito al iniciar sesión", Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    // Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    // updateUI(null)
+                }
+            }
+    }
+}
